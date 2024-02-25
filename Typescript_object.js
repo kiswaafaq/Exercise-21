18204adddb07963cfff68fcf6282b43b0b223b81
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-24 22:14:51 -0800

    first commit

---
+++ Typescript_object.js
@@ -0,0 +1,14 @@
+"use strict";
+let person = {
+    Age: 16,
+    Name: 'Kiswa',
+    Nationality: 'Pakistan',
+    Student: true
+};
+console.log(person);
+let car = {
+    Brand: 'Toyota',
+    Color: 'Black',
+    Automatic: true
+};
+console.log(car);
---
+++ Typescript_object.ts
@@ -0,0 +1,32 @@
+
+interface person {
+            Age: number ,
+            Name: string ,
+            Nationality: string ,
+            Student: boolean
+}
+
+let person = {
+          Age:16 ,
+          Name : 'Kiswa',
+          Nationality : 'Pakistan',
+          Student : true
+}
+
+console.log(person);
+
+interface car
+ {
+        Brand: string ,
+        Color: string ,
+        Automatic: boolean
+}
+
+let car =  
+{
+    Brand: 'Toyota',
+    Color: 'Black',
+    Automatic: true
+}
+
+console.log(car);
